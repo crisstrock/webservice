c423877b379beb1fe06f01cdbdb11a19f19eb4df
Cristian <cristianruiz1195@gmail.com>
2018-06-08 20:10:15 -0500

    agragamos los allowed hosts

--- webservice/settings.py
+++ webservice/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '+-t-s-o$lj9+wh^=58hpn0wbp9gkg2fhg2!3%i6ui3@dp($xdj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://strock.pythonanywhere.com',
+    'localhost'
+        ]
 
 
 # Application definition
