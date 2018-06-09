baf5a0c5382e24c0eb7241561137f0099a65d5ff
Cristian <cristianruiz1195@gmail.com>
2018-06-08 20:34:12 -0500

    Agregamos las vistas de carrera

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,6 @@ from usuarios import views
 urlpatterns = [
     url(r'^usuarios/$', views.usuario_list),
     url(r'^usuarios/(?P<pk>[0-9]+)/$', views.usuario_detail),
+    url(r'^carreras/$', views.carrera_list),
+    url(r'^carreras/(?P<pk>[0-9]+)/$', views.carrera_detail),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -6,10 +6,11 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from usuarios.models import User
+from usuarios.models import User, Carrera
 from usuarios.serializers import UsuarioSerializer
 
 # Create your views here.
+#------------------------Usuarios----------------------------------
 @csrf_exempt
 def usuario_list(request):
     """
@@ -52,4 +53,50 @@ def usuario_detail(request, pk):
 
     elif request.method == 'DELETE':
         usuario.delete()
+        return HttpResponse(status=204)
+
+#---------------CArrera------------------------------------
+
+@csrf_exempt
+def carrera_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        carreras = Carrera.objects.all()
+        serializer = CarreraSerializer(carreras, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CarreraSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def carrera_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        carrera = Carrera.objects.get(pk=pk)
+    except Carrera.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CarreraSerializer(carrera)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CarreraSerializer(carrera, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        carrera.delete()
         return HttpResponse(status=204)
